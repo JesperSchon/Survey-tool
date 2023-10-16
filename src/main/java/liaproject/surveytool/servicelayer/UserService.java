49cb4f687c8cc0bd1230eeefc2ad9c324c9d126a
schon <schon.jesper@gmail.com>
2023-10-16 15:42:09 +0200

    Skapar UserService.

--- src/main/java/liaproject/surveytool/servicelayer/UserService.java
+++ src/main/java/liaproject/surveytool/servicelayer/UserService.java
@@ -5,6 +5,8 @@ import liaproject.surveytool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -14,4 +16,19 @@ public class UserService {
   public User createUser(User user){
     return userRepository.save(user);
   }
+
+  public void deleteUser(Long userId){
+    userRepository.deleteById(userId);
+  }
+
+  public List<User> getAllUsers() {
+    return userRepository.findAll();
+  }
+
+  public User getUserById(Long userId){
+    return userRepository.findById(userId).orElse(null);
+  }
+
+
+
 }
